4ea25a03e7b798ebb090497184a5e37b18310ba6
pablo-1991 <pablo-91@live.com.ar>
2022-07-31 17:56:09 -0300

    version 2022.07.31

--- javascript/index.js
+++ javascript/index.js
@@ -21,9 +21,52 @@ let Nombre = prompt("tu nombre?")
 if ((Nombre != "") && ((Nombre == "PABLO") || (Nombre == "Pablo") || (Nombre == "pablo"))) { alert("hola Pablo!") }
 else { alert("error") } */
 
-// --------------------------------DESAFÍO COMPLEMENTARIO----------------------------------
+/*function solicitarNombre() {
+    let ingreseNombre = prompt("ingrese su nombre");
+    let ingreseApellido = prompt("ingrese apellido:")
+    alert("hola " + ingreseNombre + " " + ingreseApellido)
+}
 
-let numero = prompt("Ingrese un número:")
+solicitarNombre();*/
+
+/*let resultado = multiplicacion(10, 4)
+function multiplicacion(num1, num2) {
+    return num1 * num2
+}
+
+function mostrar() { alert("El resultado es: " + resultado) }
+
+mostrar()*/
+
+/*function calculadora(num1, num2, operador) {
+    switch (operador) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default: 
+            return 0;
+    }
+}
+
+alert(calculadora(2, 3, "/"))*/
+
+/*let yo = {
+    nombre: "Pablo",
+    edad: 31,
+    dni: 35508618,
+    peso: "64Kg",
+}
+console.log (yo)
+alert (yo.peso)*/
+
+// --------------------------------DESAFÍO COMPLEMENTARIO-------------------------------
+
+/*let numero = prompt("Ingrese un número:")
 for (let i = 1; i < 10; i++) {
     let resultado = numero * i;
     alert(numero + " X " + i + " = " + resultado);
@@ -33,4 +76,70 @@ let ingresar = prompt("Ingrese una palabra:").toLocaleLowerCase()
 while (ingresar != "salir") {
     alert("Ingresaste: " + ingresar);
     ingresar = prompt("Ingrese otra palabra:")
+}*/
+
+// ------------------------------DESAFIO ENTREGABLE N°1---------------------------------
+
+
+function solicitarNombre() {
+    alert("Bienvenido a Darwin Relax!");
+    let nombre = prompt("Por favor, ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    while (nombre === "" || apellido === "") {
+        alert("ERROR! Por favor ingrese nuevamente su nombre y/o apellido");
+        nombre = prompt("Por favor, ingrese su nombre:");
+        apellido = prompt("Ingrese su apellido:");
+    }
 }
+
+function pedido() {
+    let masaje;
+    do { masaje = prompt("Qué tipo de masaje necesitas? Ingresa el número correspondiente al masaje: \n 1)Masaje Descontracturante \n 2)Masaje Deportivo \n 3)Masaje Tailandes \n 4)Masaje con Acupuntura \n 5)Masaje con Ventosas"); }
+    while (masaje != 1 && masaje != 2 && masaje != 3 && masaje != 4 && masaje != 5);
+
+    switch (masaje) {
+        case "1":
+            return "masaje descontracturante";
+        case "2":
+            return "masaje deportivo";
+        case "3":
+            return "masaje tailandes";
+        case "4":
+            return "masaje acupuntura";
+        case "5":
+            return "masaje ventosas";
+    }
+}
+
+function verificarPrecio(masaje) {
+    if (masaje === "masaje descontracturante") {
+        return 1500;
+    }
+    else if (masaje === "masaje deportivo") {
+        return 1800;
+    }
+    else if (masaje === "masaje tailandes") {
+        return 2000;
+    }
+    else if (masaje === "masaje acupuntura") {
+        return 2500;
+    }
+    else if (masaje === "masaje ventosas") {
+        return 2800;
+    }
+}
+
+function cobro(masaje, precio) {
+    alert("Usted esta reservando turno para el siguiente masaje: " + masaje.toUpperCase() + "\nPrecio: $ " + precio)
+    let abonar = parseInt(prompt("Con cuanto abona? (Por favor abonar con cambio al momento de terminar la sesión)"));
+    if (abonar > precio) {
+        alert("Su vuelto es de $" + (abonar - precio));
+    } else {
+        alert("No le alcanza el dinero para realizar éste masaje.");
+    }
+}
+
+solicitarNombre()
+let masajeElegido = pedido();
+let precioFinal = verificarPrecio(masajeElegido);
+cobro(masajeElegido, precioFinal);
