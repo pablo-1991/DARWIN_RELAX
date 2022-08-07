8c43b7ba478ec2c14fffbed0161631d4d08cc177
pablo-1991 <pablo-91@live.com.ar>
2022-08-07 12:45:00 -0300

    version 2022.08.07

--- javascript/index.js
+++ javascript/index.js
@@ -146,7 +146,7 @@ cobro(masajeElegido, precioFinal);*/
 
 // --------------------------------DESAFÍO COMPLEMENTARIO N°2-------------------------------
 
-class Masaje {
+/*class Masaje {
     constructor(ID, Nombre, Precio) {
         this.ID = ID;
         this.Nombre = Nombre;
@@ -166,7 +166,102 @@ console.table(masajes)
 
 console.log(masajes.length)
 
-console.log(masajes.includes("Masaje chino"))
+console.log(masajes.includes("Masaje chino"))*/
+
+
+// --------------------------------1° ENTREGA PROYECTO FINAL--------------------------------
+
+function solicitarNombre() {
+    alert("Bienvenido a Darwin Relax!");
+    let nombre = prompt("Por favor, ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    while (nombre === "" || apellido === "") {
+        alert("ERROR! Por favor ingrese nuevamente su nombre y/o apellido");
+        nombre = prompt("Por favor, ingrese su nombre:");
+        apellido = prompt("Ingrese su apellido:");
+    }
+}
+
+function pedido() {
+    let masaje;
+    do { masaje = prompt("Qué tipo de masaje necesitas? Ingresa el número correspondiente al masaje: \n 1)Masaje Descontracturante \n 2)Masaje Deportivo \n 3)Masaje Tailandes \n 4)Masaje con Acupuntura \n 5)Masaje con Ventosas"); }
+    while (masaje != 1 && masaje != 2 && masaje != 3 && masaje != 4 && masaje != 5);
+
+    switch (masaje) {
+        case "1":
+            return "masaje descontracturante";
+        case "2":
+            return "masaje deportivo";
+        case "3":
+            return "masaje tailandes";
+        case "4":
+            return "masaje acupuntura";
+        case "5":
+            return "masaje ventosas";
+    }
+}
+
+function verificarPrecio(masaje) {
+    if (masaje === "masaje descontracturante") {
+        return 1500;
+    }
+    else if (masaje === "masaje deportivo") {
+        return 1800;
+    }
+    else if (masaje === "masaje tailandes") {
+        return 2000;
+    }
+    else if (masaje === "masaje acupuntura") {
+        return 2500;
+    }
+    else if (masaje === "masaje ventosas") {
+        return 2800;
+    }
+}
+
+function cobro(masaje, precio) {
+    alert("Usted esta reservando turno para el siguiente masaje: " + masaje.toUpperCase() + "\nPrecio: $ " + precio)
+    let abonar = parseInt(prompt("Con cuanto abona? (Por favor abonar con cambio al momento de terminar la sesión)"));
+    if (abonar > precio) {
+        alert("Su vuelto es de $" + (abonar - precio));
+    } else {
+        alert("No le alcanza el dinero para realizar éste masaje.");
+    }
+}
+
+class Masaje {
+    constructor(ID, Nombre, Precio) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+    }
+}
+
+const masajes = [];
+
+masajes.push(new Masaje("01", "Masaje descontracturante", "1500"))
+masajes.push(new Masaje("02", "Masaje deportivo", "1800"))
+masajes.push(new Masaje("03", "Masaje tailandes", "2000"))
+masajes.push(new Masaje("04", "Masaje con acupuntura", "2500"))
+masajes.push(new Masaje("05", "Masaje con ventosas", "2800"))
+
+
+const masajesPrecioActualizado = masajes.map((actualizacion) => {
+    return {
+        ID: actualizacion.ID,
+        Nombre: actualizacion.Nombre,
+        Precio: actualizacion.Precio * 1.50,
+    }
+})
+
+solicitarNombre()
+let masajeElegido = pedido();
+let precioFinal = verificarPrecio(masajeElegido);
+cobro(masajeElegido, precioFinal);
+console.table(masajes);
+console.log(masajes.length);
+console.log(masajes.includes("Masaje chino"));
+console.table(masajesPrecioActualizado);
 
 
 
