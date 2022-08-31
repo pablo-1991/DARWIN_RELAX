af5f126ee6120e4ac8caa5ca9b104063a7a3ea49
Pablo Palumbo <pablo-91@live.com.ar>
2022-08-30 21:02:53 -0300

    version 2022.08.30

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -17,15 +17,16 @@ function recibir1() {
         Swal.fire({
             icon: "info",
             title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-        });
-    
-    (parseInt(monto) == 1500) &&
+        })
+
+    if (parseInt(monto) == 1500) {
         Swal.fire({
             icon: "success",
             title: "Muchas gracias por confiar en nosotros!",
         })
-    
-    (parseInt(monto) < 1500) ||
+    }
+
+    (parseInt(monto) < 1500) &&
         Swal.fire({
             icon: "error",
             title: "No le alcanza para éste masaje",
@@ -35,148 +36,155 @@ function recibir1() {
 function recibir2() {
     let monto = document.getElementById("texto2").value;
     (parseInt(monto) > 1800) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 1800),
+        })
 
-(parseInt(monto) == 1800) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
-
-(parseInt(monto) < 1800) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    if (parseInt(monto) == 1800) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
     }
+
+    (parseInt(monto) < 1800) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
+}
 
 function recibir3() {
     let monto = document.getElementById("texto3").value;
     (parseInt(monto) > 2000) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 2000),
+        })
 
-(parseInt(monto) == 2000) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 2000) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 2000) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 2000) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 function recibir4() {
     let monto = document.getElementById("texto4").value;
     (parseInt(monto) > 2500) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 2500),
+        })
 
-(parseInt(monto) == 2500) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 2500) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 2500) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 2500) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 function recibir5() {
     let monto = document.getElementById("texto5").value;
     (parseInt(monto) > 2800) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 2800),
+        })
 
-(parseInt(monto) == 2800) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 2800) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 2800) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 2800) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 function recibir6() {
     let monto = document.getElementById("texto6").value;
     (parseInt(monto) > 1600) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 1600),
+        })
 
-(parseInt(monto) == 1600) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 1600) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 1600) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 1600) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 function recibir7() {
     let monto = document.getElementById("texto7").value;
     (parseInt(monto) > 3000) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 3000),
+        })
 
-(parseInt(monto) == 3000) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 3000) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 3000) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 3000) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 function recibir8() {
     let monto = document.getElementById("texto8").value;
     (parseInt(monto) > 2600) &&
-    Swal.fire({
-        icon: "info",
-        title: "Su vuelto es de $" + (parseInt(monto) - 1500),
-    });
+        Swal.fire({
+            icon: "info",
+            title: "Su vuelto es de $" + (parseInt(monto) - 2600),
+        })
 
-(parseInt(monto) == 2600) &&
-    Swal.fire({
-        icon: "success",
-        title: "Muchas gracias por confiar en nosotros!",
-    })
+    if (parseInt(monto) == 2600) {
+        Swal.fire({
+            icon: "success",
+            title: "Muchas gracias por confiar en nosotros!",
+        })
+    }
 
-(parseInt(monto) < 2600) ||
-    Swal.fire({
-        icon: "error",
-        title: "No le alcanza para éste masaje",
-    })
+    (parseInt(monto) < 2600) &&
+        Swal.fire({
+            icon: "error",
+            title: "No le alcanza para éste masaje",
+        })
 }
 
 cantidadClicks();
